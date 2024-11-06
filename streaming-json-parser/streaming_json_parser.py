4d36847d95371e594f43c9b206869444de8c2177
MPBaptista <miguelbaptista1000@gmail.com>
2024-11-06 00:55:07 +0000

    feature: add docstrings

--- streaming-json-parser/streaming_json_parser.py
+++ streaming-json-parser/streaming_json_parser.py
@@ -2,6 +2,12 @@ import unittest
 
 class StreamingJsonParser:
     def __init__(self):
+        """
+        Initializes a StreamingJsonParser object.
+
+        The object is initialized with empty stack, no current key or value,
+        not in a string, no partial string, and an empty result.
+        """
         self.stack = []
         self.current_key = None
         self.current_value = None
@@ -10,6 +16,16 @@ class StreamingJsonParser:
         self.result = {}
         
     def consume(self, buffer: str):
+        """
+        Consumes a string buffer and attempts to parse it.
+
+        The buffer is parsed by iterating through the characters in the buffer.
+        For each character, if it is a special character, it is handled by
+        calling the corresponding method. If the character is not special, it is
+        handled by calling the _handle_non_special_character method.
+
+        :param buffer: The string buffer to be parsed
+        """
         actions = {
             '{': lambda: self._handle_start_object(),
             '}': lambda: self._handle_end_object(),
@@ -20,15 +36,32 @@ class StreamingJsonParser:
 
         for char in buffer:
             if char not in actions:
+                # If the character is not special, handle it
                 self._handle_non_special_character(char)
             else:
+                # If the character is special, call the corresponding method
                 actions[char]()
 
     def _handle_start_object(self):
+        """
+        Handles a '{' character in the input buffer.
+
+        When the '{' character is encountered, if the stack is empty, a new
+        object is created and pushed onto the stack.
+        """
         if not self.stack:
             self.stack.append({})
 
     def _handle_end_object(self):
+        """
+        Handles a '}' character in the input buffer.
+
+        This method finalizes the most recently opened JSON object. If there is a
+        current key and value, they are added to the stack. The completed object is
+        then popped from the stack. If there are still objects on the stack, the
+        completed object is assigned to the current key of the next object in the
+        stack. If the stack is empty, the completed object becomes the result.
+        """
         if self.stack:
             if self.current_key and self.current_value is not None:
                 self.stack[-1][self.current_key] = self.current_value
@@ -46,6 +79,14 @@ class StreamingJsonParser:
         pass
     
     def _handle_quote(self):
+        """
+        Handles a '"' character in the input buffer.
+
+        If the parser is in a string (i.e. self.in_string is True), the
+        current key or value is set to the partial string and the partial
+        string is reset. If the parser is not in a string, the parser is
+        set to be in a string and the partial string is reset.
+        """
         if self.in_string:
             if self.current_key is None:
                 self.current_key = self.partial_string
@@ -58,12 +99,26 @@ class StreamingJsonParser:
             self.partial_string = ""
             
     def _handle_comma(self):
+        """
+        Handles a ',' character in the input buffer.
+
+        If the current key and value are not None, they are added to the
+        current object in the stack. The current key and value are then reset.
+        """
         if self.current_key and self.current_value is not None:
             if self.stack:
                 self.stack[-1][self.current_key] = self.current_value
             self.current_key, self.current_value = None, None
 
     def _handle_non_special_character(self, char):
+        """
+        Handles a non-special character in the input buffer.
+
+        If the parser is in a string (i.e. self.in_string is True), the
+        character is added to the partial string. If the parser is not in a
+        string, the character is added to the current value if it is not
+        None, otherwise it is set as the current value.
+        """
         if self.in_string:
             self.partial_string += char
         elif not self.in_string and char.strip():
@@ -73,6 +128,15 @@ class StreamingJsonParser:
                 self.current_value += char
 
     def get(self) -> dict: 
+        """
+        Returns the parsed JSON object as a dictionary.
+
+        The method checks if there are any remaining key-value pairs or
+        strings that need to be added to the result. It then returns the
+        result.
+
+        :return: The parsed JSON object as a dictionary
+        """
         if self.current_key and self.current_value is not None and self.stack:
             self.stack[-1][self.current_key] = self.current_value
             self.current_key, self.current_value = None, None
@@ -126,11 +190,6 @@ class TestStreamingJsonParser(unittest.TestCase):
         parser = StreamingJsonParser()
         parser.consume('{"tes"')
         assert parser.get() == {"tes": ""}
-
-    def test_nested_streaming_json_parser(self):
-        parser = StreamingJsonParser()
-        parser.consume('{"foo": {"foo":"bar"}}')
-        assert parser.get() == {"foo": {"foo":"bar"}}
 
     def test_suffix_streaming_json_parser(self):
         parser = StreamingJsonParser()
